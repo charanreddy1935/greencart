b92c37b22e7ba1c524b2f06b6cfee6d4b9565f6b
charanreddy1935 <charanreddy1935@gmail.com>
2025-07-22 16:44:04 +0530

    commit2

--- server/server.js
+++ server/server.js
@@ -20,7 +20,11 @@ await connectDB();
 await connectCloudinary();
 
 
+
 const allowedOrigins=['http://localhost:5173']
+
+
+
 
 app.post('/stripe',express.raw({type:application/json}),stripeWebHooks)
 
